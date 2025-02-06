cf8665731f1781f359c6f9f8afdae19bd7d8b6d3
Cillian Barron <barroncillian@gmail.com>
2025-02-06 21:25:59 +0000

    feat: add skew protection

--- astro.config.ts
+++ astro.config.ts
@@ -11,8 +11,14 @@ import typesafeRoutes from "astro-typesafe-routes";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
-  integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  }), typesafeRoutes()],
+  adapter: vercel({
+    skewProtection: true,
+  }),
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    typesafeRoutes(),
+  ],
 });
